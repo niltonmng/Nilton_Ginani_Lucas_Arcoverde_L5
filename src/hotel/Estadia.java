e1535c9d08b86b34de8d5b58efba9b699a17f587
Nilton Ginani <nilton.ginani@ccc.ufcg.edu.br>
2016-08-09 08:42:02 -0300

    Modifiquei a estadia

--- src/hotel/Estadia.java
+++ src/hotel/Estadia.java
@@ -7,8 +7,8 @@ public class Estadia {
 	private int dias;
 	
 	public Estadia(String nome, String tipo, int idade, int dias, double valor) throws Exception{
-		if(valor <= 0){
-			throw new Exception("O valor nao pode ser menor ou igual a zero.");
+		if(valor < 0){
+			throw new Exception("O valor nao pode ser menor que zero.");
 		}
 		if(dias <= 0){
 			throw new Exception("A quantidade de dias nao pode ser menor ou igual a zero.");
